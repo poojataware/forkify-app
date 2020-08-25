52489fc9d9af33975c658198e99ca29018aa9f08
poojataware <65983983+poojataware@users.noreply.github.com>
2020-08-25 20:21:40 +0200

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,16 @@
-// Global app controller
+
+import axios from 'axios';
+async function getResults(query) {
+    try{
+        const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${query}`); 
+        const result=res.data.recipes;
+        console.log(result);
+
+    }
+    catch(error){
+        alert(error);
+    }
+    
+
+}
+getResults('ice cream');
