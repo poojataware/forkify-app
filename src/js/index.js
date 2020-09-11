01fd25bb78b5b958ba582afa6db7f7c045605fd4
poojataware <65983983+poojataware@users.noreply.github.com>
2020-09-11 16:04:00 +0200

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,6 @@
 import Search from './models/Search';
+import {elements,renderLoader,clearLoader} from './views/base';
+import * as searchView from './views/searchView';
 /**Global state of the app
  * -Search object
  * -Current recipe object
@@ -9,21 +11,32 @@ const state={};
 
 const controlSearch = async () => {
     //1)Get query from view
-    const query = 'pizza'//TODO
+    const query = searchView.getInput();//TODO
     if(query){
         //2)new search object and add to state
         state.search = new Search(query);
         //3)prepare a UI for result
-        
+        searchView.clearInput();
+        searchView.clearResults();
+        renderLoader(elements.searchRes);
         //4)search for recipes 
         await state.search.getResults();
         //5)Render results on UI
-        console.log(state.search.result);
+        clearLoader();
+        searchView.renderResults(state.search.result);
     }
 }
-document.querySelector('.search').addEventListener('submit', e=>{
+elements.searchForm.addEventListener('submit', e=>{
     e.preventDefault();
     controlSearch();
     
 });
 
+elements.searchResPages.addEventListener('click', e => {
+    const btn=e.target.closest('.btn-inline');
+    if(btn){
+        const goToPage = parseInt(btn.dataset.goto, 10);
+        searchView.clearResults();
+        searchView.renderResults(state.search.result, goToPage);        
+    }
+});
