dc61bb164693ef1ad60efeb6293fb5100ae86444
poojataware <65983983+poojataware@users.noreply.github.com>
2020-09-12 12:26:06 +0200

    Update searchView.js

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -25,54 +25,58 @@ const limitRecipeTitle = (title, limit = 17) => {
 
 }
 
+
 const renderRecipe = recipe => {
-    const markup =
-        `
-    <li>
-    <a class="results__link" href="#${recipe.recipe_id}">
-        <figure class="results__fig">
-            <img src="${recipe.image_url}" alt=${recipe.title}">
-        </figure>
-        <div class="results__data">
-            <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
-            <p class="results__author">${recipe.publisher}</p>
-        </div>
-    </a>
-</li>
-`;
+    const markup = `
+        <li>
+            <a class="results__link" href="#${recipe.recipe_id}">
+                <figure class="results__fig">
+                    <img src="${recipe.image_url}" alt="${recipe.title}">
+                </figure>
+                <div class="results__data">
+                    <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
+                    <p class="results__author">${recipe.publisher}</p>
+                </div>
+            </a>
+        </li>
+    `;
     elements.searchResList.insertAdjacentHTML('beforeend', markup);
 };
-const createButton=(page,type)=>
-    `<button class="btn-inline results__btn--${type}" data-goto=${type === 'prev' ? page - 1 : page + 1}>
-    <svg class="search__icon">
-        <use href="img/icons.svg#icon-triangle-${type === 'prev' ? 'left' : 'right'}"></use>
-    </svg>
-    <span>Page ${type === 'prev' ? page - 1 : page + 1}</span>
-</button>
-    `;
-const renderButton=(page,numResults,resPerPage)=>{
-    const pages=Math.ceil(numResults/resPerPage);
+
+    const createButton = (page, type) => `
+    <button class="btn-inline results__btn--${type}" data-goto=${type === 'prev' ? page - 1 : page + 1}>
+        <span>Page ${type === 'prev' ? page - 1 : page + 1}</span>
+        <svg class="search__icon">
+            <use href="img/icons.svg#icon-triangle-${type === 'prev' ? 'left' : 'right'}"></use>
+        </svg>
+    </button>
+`;
+
+const renderButton = (page, numResults, resPerPage) => {
+    const pages = Math.ceil(numResults / resPerPage);
+
     let button;
-    if(page===1 && pages>1){
-        //only button to go to next page
-        button=createButton(page, 'next');
-    }else if(page<pages){
-        //both button
-        button=`
+    if (page === 1 && pages > 1) {
+        // Only button to go to next page
+        button = createButton(page, 'next');
+    } else if (page < pages) {
+        // Both buttons
+        button = `
             ${createButton(page, 'prev')}
             ${createButton(page, 'next')}
         `;
-    }else if(page===pages && pages>1){
-        //only button to go to previous page
-        button=createButton(page, 'prev');
+    } else if (page === pages && pages > 1) {
+        // Only button to go to prev page
+        button = createButton(page, 'prev');
     }
-    elements.searchResPages.insertAdjacentHTML('afterbegin',button);
+
+    elements.searchResPages.insertAdjacentHTML('afterbegin', button);
 };
 
 export const renderResults =(recipes, page=1, resPerPage=10) => {
     //render results of current page
     const start=(page-1)*resPerPage;
-    const end=9;
+    const end=page * resPerPage;
     recipes.slice(start,end).forEach(renderRecipe);
 
     //render pagination buttons
